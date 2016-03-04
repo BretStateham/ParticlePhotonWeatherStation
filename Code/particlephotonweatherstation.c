239cd65490f8a39ffbb3f893c11a78f43ec73f69
Bret Stateham <Bret.Stateham@microsoft.com>
2016-03-04 10:56:18 -0800

    Cleaned up code

--- Code/particlephotonweatherstation.c
+++ Code/particlephotonweatherstation.c
@@ -19,41 +19,46 @@ char Org[] = "ORGANIZATION_NAME";
 char Disp[] = "DISPLAY_NAME";
 char Locn[] = "LOCATION";
 
+//The amount of time (in milliseconds) to wait between each 
+//publication of data via the Particle WebHook
+//Particle webhooks are rate limited to a maximum of 10/minute/device
+//if we can only send 10 times a minute, that means we can send on average
+//about once every six seconds, or 6000 milliseconds.  So to avoid any 
+//rate limits by the particle cloud, keep the sendDelay at at least 6000.
+int sendDelay = 6000;
 
 void setup()
 {
-    dht.begin();
-    delay(10000);
+  dht.begin();
+  delay(10000);
 }
 
  
 void loop()
 {
-    delay(5000);
-    
-// Reading temperature or humidity takes about 250 milliseconds!
-// Sensor readings may also be up to 2 seconds 'old' (its a 
-// very slow sensor)
-	float h = dht.getHumidity();
-// Read temperature as Celsius
-    float t = dht.getTempCelcius();
-// Read temperature as Farenheit
-	float f = dht.getTempFarenheit();
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a 
+  // very slow sensor)
+  float h = dht.getHumidity();
+  // Read temperature as Celsius
+  float t = dht.getTempCelcius();
+  // Read temperature as Farenheit
+  float f = dht.getTempFarenheit();
   
-// Check if any reads failed and exit early (to try again).
-//	if (isnan(h) || isnan(t) || isnan(f)) {
-//		Serial.println("Failed to read from DHT sensor!");
-//		return;
-//	}   
-   
-    char payload[255];
+  //Generate the temperature data payload
+  char payload[255];
+  snprintf(payload, sizeof(payload), "{ \"s\":\"wthr\", \"u\":\"F\",\"l\":\"%s\",\"m\":\"Temperature\",\"o\":\"%s\",\"v\": %f,\"d\":\"%s\" }", Locn, Org, f, Disp);
+  //Send the temprature data payload
+  Spark.publish("PublishToEventHub", payload);
+  //Wait for the specified "sendDelay" before sending the humidity data...    
+  delay(sendDelay);
     
-    snprintf(payload, sizeof(payload), "{ \"s\":\"wthr\", \"u\":\"F\",\"l\":\"%s\",\"m\":\"Temperature\",\"o\":\"%s\",\"v\": %f,\"d\":\"%s\" }", Locn, Org, f, Disp);
-    Spark.publish("ConnectTheDots", payload);
     
-    delay(5000);
-    
-    snprintf(payload, sizeof(payload), "{ \"s\":\"wthr\", \"u\":\"%%\",\"l\":\"%s\",\"m\":\"Humidity\",\"o\":\"%s\",\"v\": %f,\"d\":\"%s\" }", Locn, Org, h, Disp);
-    Spark.publish("ConnectTheDots", payload);
-    
+  //Generate the humidity data payload
+  snprintf(payload, sizeof(payload), "{ \"s\":\"wthr\", \"u\":\"%%\",\"l\":\"%s\",\"m\":\"Humidity\",\"o\":\"%s\",\"v\": %f,\"d\":\"%s\" }", Locn, Org, h, Disp);
+  //Send the humidity data payload
+  Spark.publish("PublishToEventHub", payload);
+  //wait for the specified "sendDelay" before looping...
+  delay(sendDelay);
 }
